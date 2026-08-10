89b4b69c3009986046cee885f50da75c6340c605
Guilherme Simionato <devsanom@gmail.com>
2026-08-10 12:57:43 +0000

    Initial commit with few classes done: Status 200, Postman, Designing urls, Versioning APIs, Authentication Tokens and Revoking Tokens

---
+++ routes/api_v1.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:sanctum')->apiResource('tickets', \App\Http\Controllers\Api\V1\TicketController::class);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
